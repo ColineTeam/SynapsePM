89cbbf70fe00edfe89a67cb1989e01a078aad0e3
Aleksey Lozovagin <Oleksih@gmail.com>
2018-06-09 15:02:19 +0300

    Update SynapseInterface

--- src/synapsepm/network/SynapseInterface.php
+++ src/synapsepm/network/SynapseInterface.php
@@ -22,7 +22,56 @@ class SynapseInterface {
         $this->registerPackets();
         $this->client = new SynapseClient(Server::getInstance()->getLogger(), $port, $ip);
     }
-    public function registerPackets(){
+    public function getPacket($pid, $buffer){
+        $class = $this->packetPool[$pid];
+        if ($class !== null) {
+            $pk = clone $class;
+            $pk->setBuffer($buffer, 1);
+            return $pk;
+        }
+        return null;
+    }
+    /* @var $packet - SynapseDataPacket*/
+    public function registerPacket($id, $packet){
+        $this->packetPool[$id] = $packet;
+    }
+    public function getSynapse(){
+        return $this->synapse;
+    }
+    public function reconnect(){
+        $this->client->reconnect();
+    }
+    public function getPutPacketThread(){
+        return $this->putPacketThread;
+    }
+    public function putPacket($pk){
 
+    }
+    public function isConnected(){
+        return $this->connected;
+    }
+    public function process(){
+        $pk = $this->client->readMainToThreadPacket();
+        while ($pk !== null){
+            $this->handlePacket($pk);
+            $pk = $this->client->readMainToThreadPacket();
+        }
+
+        $this->connected = $this->client->isConnected();
+        if($this->connected && $this->client->isNeedAuth()){
+            $this->synapse->connect();
+            $this->client->setNeedAuth(false);
+        }
+    }
+    public function handlePacket($pk){
+        if($pk !== null){
+            $pk->decode();
+            $this->synapse->handleDataPacket($pk);
+        }
+    }
+    public function registerPackets(){
+        $this->packetPool = new \SplFixedArray(256);
+
+        //packets
     }
 }
