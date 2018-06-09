00c01b99728a6ef6efb9aa5113ae0b3a2d4d2d3c
Daniil Link <thegame200000@gmail.com>
2018-06-09 19:03:34 +0700

    Create SynapseDataPacket.php

---
+++ src/synapsepm/network/spp/SynapseDataPacket.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace synapseapi\network\protocol\spp;
+
+use pocketmine\utils\BinaryStream;
+
+abstract class SynapseDataPacket extends BinaryStream {
+
+    public $isEncoded = false;
+
+    abstract public function pid();
+
+    abstract public function decode();
+
+    abstract public function encode();
+
+    public function reset() {
+        parent::reset();
+    }
+
+    public function clean() {
+        $this->setBuffer(null);
+
+        $this->isEncoded = false;
+        $this->offset = 0;
+        return $this;
+    }
+
+    public function clone() {
+        try {
+            return new parent;
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+
+}
