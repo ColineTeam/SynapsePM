7ba52a17f17a8e730a479973c506ab43cd5ef0c1
Daniil Link <thegame200000@gmail.com>
2018-06-10 20:42:56 +0700

    Create SynapseSlientEvent.php

---
+++ src/synapsepm/event/client/SynapseSlientEvent.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace synapsepm\event\client;
+
+use synapsepm\event\SynapseEvent;
+
+/**
+ * Created by boybook on 16/6/25.
+ */
+abstract class SynapseSlientEvent extends SynapseEvent {
+    
+}
