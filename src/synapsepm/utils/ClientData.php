c3a78c775cd99e7dd04c1da2a4e564569767b0a8
Daniil Link <thegame200000@gmail.com>
2018-06-10 21:02:12 +0700

    Create ClientData.php

---
+++ src/synapsepm/utils/ClientData.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace synapseapi\utils;
+
+/**
+ * Created by boybook on 16/6/25.
+ */
+class ClientData {
+
+    public $clientList = array();
+
+    public function getHashByDescription($description) {
+        $re = null;
+        foreach($this->clientList as $hash => $entry){
+            if ($entry->getDescription() == $description) {
+                $re = $hash;
+            }
+        }
+        return $re;
+    }
