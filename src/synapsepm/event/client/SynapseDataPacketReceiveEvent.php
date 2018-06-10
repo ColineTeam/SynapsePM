d06f6012fb1b49b9e824c58f696d5088114da68d
Daniil Link <thegame200000@gmail.com>
2018-06-10 20:49:04 +0700

    Create SynapseDataPacketReceiveEvent.php

---
+++ src/synapsepm/event/client/SynapseDataPacketReceiveEvent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace synapsepm\event\client;
+
+use pocketmine\event\Cancellable;
+use pocketmine\event\HandlerList;
+use synapsepm\SynapseEntry;
+use synapsepm\event\SynapseEvent;
+use synapsepm\network\protocol\spp\SynapseDataPacket;
+
+/**
+ * @author CreeperFace
+ */
+class SynapseDataPacketReceiveEvent extends SynapseEvent implements Cancellable {
+
+    public static $handlerList = null;
+    private final $packet;
+    private final $entry;
+
+    public function __construct(SynapseEntry $entry, SynapseDataPacket $packet) {
+        $this->packet = $packet;
+        $this->entry = $entry;
+    }
+
+    public function getPacket() {
+        return $this->packet;
+    }
+}
