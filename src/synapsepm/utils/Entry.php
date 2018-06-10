44decbe47fdc9d0fb0b1b039b44c6a76767be5c9
Daniil Link <thegame200000@gmail.com>
2018-06-10 20:54:17 +0700

    Create Entry.php

---
+++ src/synapsepm/utils/Entry.php
@@ -0,0 +1,29 @@
+<?php
+
+class Entry {
+    private $ip;
+    private $port;
+    private $playerCount;
+    private $maxPlayers;
+    private $description;
+
+    public function getIp() {
+        return $this->ip;
+    }
+
+    public function getPort() {
+        return $this->port;
+    }
+
+    public function getMaxPlayers() {
+        return $this->maxPlayers;
+    }
+
+    public function getPlayerCount() {
+        return $this->playerCount;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+}
